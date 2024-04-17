63c3510a35d28db71558009119dd4ce0df78bbeb
Felix <yannanj11@gmail.com>
2024-04-17 20:34:59 +1000

    update

--- config.py
+++ config.py
@@ -3,11 +3,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    @staticmethod
+    def get_db_uri():
+        res = os.environ.get('DATABASE_URL')
+        if res:
+            res = res.replace('postgres://', 'postgresql://')
+        else:
+            res = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+        return res
+    
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'proudly-developed-by-NotNull-team'
-    # SQLALCHEMY_DATABASE_URI = 'mysql://root:rootpassword@db/mydatabase'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
-        'postgres://', 'postgresql://')  or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = get_db_uri()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     IMAGE_FOLDER = os.path.join(basedir, 'app', 'static', 'images')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
